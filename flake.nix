1787fd5384d1051ff78f2f068d09f944dad8f9cb
Alan Urmancheev <alan.urman@gmail.com>
2025-07-02 18:15:28 +0000

    flake.nix: format

--- flake.nix
+++ flake.nix
@@ -1,26 +1,31 @@
 {
   description = "Script to display large files in a directory";
 
-  outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.default = pkgs.stdenv.mkDerivation {
-      name = "lsbig";
-      src = ./.;
-      nativeBuildInputs = with pkgs; [
-        makeWrapper
-        installShellFiles
-        scdoc
-      ];
-      installPhase = ''
-        installBin lsbig
-        scdoc < lsbig.1.scd > lsbig.1
-        installManPage lsbig.1
-        wrapProgram $out/bin/lsbig --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
-          coreutils
-          gawk
-        ])}
-      '';
-    };
-  });
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "lsbig";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [
+          makeWrapper
+          installShellFiles
+          scdoc
+        ];
+        installPhase = ''
+          installBin lsbig
+          scdoc < lsbig.1.scd > lsbig.1
+          installManPage lsbig.1
+          wrapProgram $out/bin/lsbig --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
+            coreutils
+            gawk
+          ])}
+        '';
+      };
+    });
 }
